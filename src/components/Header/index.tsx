50439e4fd76e720cf068eb7f7ed5538647c8635a
Jose Martins <jose.melo@doutorhoje.com>
2021-05-16 22:11:38 -0300

    [FINALIZED] Projeto Finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
+import { ActiveLink } from '../AtiveLink';
 
 export function Header(){
   return (
@@ -8,8 +9,13 @@ export function Header(){
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="logo ignews"/>
         <nav >
-          <a href="" className={styles.active}>Home</a>
-          <a href="">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active}  href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
+
         </nav>
 
         <SignInButton />
